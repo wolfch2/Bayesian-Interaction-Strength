56a2bf4c06dc4fbdb98bdaed40e1c7d5f831b283
wolfch2 <wolfch@onid.orst.edu>
2016-03-17 13:30:46 -0700

    Added size class split

---
+++ size_class_plots.r
@@ -0,0 +1,103 @@
+# Separate script showing how attack rate estimation procedure can be applied to subsets of the feeding data.
+
+#########################################################
+######################################################### 1. setup (user may have to modify this seciton)
+#########################################################
+
+setwd("C:/Users/Chris/Dropbox/stat phd/project_code - whelk update") # contains folders: "scripts", "data", "models", and "output"
+nsamples = 1000 # number of posterior (and bootstrap) samples to obtain
+
+# use install.packages(...) if any of these packages are not already installed
+require(MASS) # need for mvrnorm
+require(MCMCpack) # need for rwish
+require(mvnmle) # needed to est. multivar norm params.
+require(mvtnorm)
+require(numDeriv)
+require(Matrix)
+require(mcmc)
+require(rjags) # call jags from R
+require(coda) # analyze output in R
+require(ggplot2)
+require(reshape2)
+require(grid)
+require(gridExtra)
+require(lattice)
+require(colorspace)
+require(RColorBrewer)
+require(plyr)
+require(scales)
+require(Hmisc)
+
+#########################################################
+######################################################### 2. load and process data
+#########################################################
+
+options("stringsAsFactors"=FALSE) # turn off automatic factors on data import
+
+raw_data = list(
+	feed = read.csv("data/NZ-FeedingObservations-Select.csv"),
+	htimes_reg  = read.csv("data/NZ-HandlingTimes-MRegnCoeff-MeasuredANDMatched.csv",
+		skip=3),
+	htimes = read.csv("data/NZ-HandlingTimes.csv"),
+	temps = read.csv("data/NZ-Temps-Monthly.csv"),
+	quads = read.csv("data/NZ-SpeciesDensities-Select.csv")
+)
+
+# prepare feeding survey, handling time, and abundance data for analysis
+source("scripts/process_data.r")
+data = process_data(raw_data)
+
+quantile_table = table(data$feed$Prey, cut(data$feed$PredSize, quantile(data$feed$PredSize, seq(0,1,by=1/8))))
+quantile_table = rbind(quantile_table, `Total Surveyed`=apply(quantile_table,2,sum))
+tmp = latex(quantile_table, file = "output/quantile_table.tex", table.env=FALSE, booktabs=TRUE)
+
+# split feeding survey data by predator size into eight groups (of roughly equal size)
+feed_split = split(data$feed, cut(data$feed$PredSize, quantile(data$feed$PredSize, seq(0,1,by=1/8))))
+
+# load attack rate estimation procedures (just using Bayes here)
+methods_files = c("bayes.r", "nonpara_bs.r", "para_bs.r")
+sapply(paste0("scripts/", methods_files), source)
+
+results_list = sapply(feed_split, function(feed_subset){
+       data$feed = feed_subset
+       results = bayesian_method(data) # apply Bayesian method to each subset separately
+})
+names(results_list) = sapply(feed_split,function(x) median(x$PredSize)) # median pred sizes in each group
+
+saveRDS(results_list, "output/results_list_8classes.RDS") # save results for future use (optional)
+# results_list = readRDS("output/results_list_8classes.RDS")
+
+#########################################################
+######################################################### plot
+#########################################################
+
+data = do.call("rbind",sapply(names(results_list),function(x) data.frame(Size=x,results_list[[x]][,c("Chamaesipho columna","Xenostrobus pulex")]), simplify=FALSE))
+data = melt(data,id.vars="Size")
+data$value = log(data$value,base=10)
+
+# haven't had much success w/ geom_violin using cont. x and grouping var.  So, just use discrete and add a "blank" group
+data2 = rbind(cbind(data,alpha=1),
+	data.frame(Size=10,variable="Chamaesipho.columna",value=-5.5,alpha=0))
+
+data2$Size = factor(as.numeric(data2$Size),
+	levels=sort(unique(as.numeric(data2$Size))))
+
+data2$variable = gsub("\\.", " ", data2$variable)
+
+dev.new(width=7,height=6)
+ggplot(data2, aes(x=Size,y=value)) +
+	geom_violin(fill="gray") +
+	facet_wrap(~ variable, scales="free_y") +
+	theme_bw()  +
+	stat_summary(aes(group=Size,alpha=alpha), fun.y=median, geom="point") +
+	scale_alpha(range=c(0,1), guide=FALSE) +
+	xlab("Median predator size (mm)") +
+	theme(strip.text = element_text(size = 10, face = "italic", angle = 0)) +
+	scale_y_continuous(labels=trans_format("I", math_format(10^.x))) +
+	ylab(bquote("Attack Rate (" ~ xi[i] ~ ")"))
+savePlot(filename = "output/size_class.pdf", type = "pdf")
+savePlot(filename = "output/size_class.png", type = "png")
+dev.off()
+
+cat("\r") # follow w/ blank line to reset cursor
+
